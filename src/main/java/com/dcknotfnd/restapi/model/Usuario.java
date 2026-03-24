1f52d700619e547544c3b7838c4ca5309d77cf34
bayron-baez <sergionbaezbarria44@gmail.com>
2026-03-24 12:17:17 -0300

    feat: implementación de modelos, repositorios y controladores para Usuario y Mascota
    Se creó la estructura base del CRUD con JPA para las entidades Usuario y Mascota. Además, se integró la clase utilitaria JwtUtil y el endpoint inicial de login para la autenticación.

--- src/main/java/com/dcknotfnd/restapi/model/Usuario.java
+++ src/main/java/com/dcknotfnd/restapi/model/Usuario.java
@@ -1,4 +1,65 @@
 package com.dcknotfnd.restapi.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    //ej ciudadano, clinica, refugio , municipalidad
+    @Column(nullable = false)
+    private String rol;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
--- src/main/java/com/dcknotfnd/restapi/repository/UsuarioRepository.java
+++ src/main/java/com/dcknotfnd/restapi/repository/UsuarioRepository.java
@@ -1,4 +1,11 @@
 package com.dcknotfnd.restapi.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.dcknotfnd.restapi.model.Usuario;
+@Repository
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    // JPA crea la consulta SQL automáticamente solo con nombrar bien el método
+    Usuario findByEmail(String email);
 }
