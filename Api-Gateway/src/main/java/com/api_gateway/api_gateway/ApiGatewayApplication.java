b9946f37aeb2a5c0dd1fba0973bc4ca731992d70
valdebenito <cr.valdebenitop@duocuc.cl>
2026-03-29 20:01:37 -0300

    Cracion de api-gateway desde 0, sigue con el error de endpoint 404
    estoy chato de este codigo, gracias luxin :D

--- Api-Gateway/src/main/java/com/api_gateway/api_gateway/ApiGatewayApplication.java
+++ Api-Gateway/src/main/java/com/api_gateway/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.api_gateway.api_gateway;
+package com.api_gateway.Api_Gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
 }
--- Api-Gateway/src/test/java/com/api_gateway/api_gateway/ApiGatewayApplicationTests.java
+++ Api-Gateway/src/test/java/com/api_gateway/api_gateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.api_gateway.api_gateway;
+package com.api_gateway.Api_Gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
