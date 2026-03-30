e552521901ae0aff5c9875fd86f7b8fbe95702f2
Gustavo Santana <gustavosantanafue@gmail.com>
2026-03-30 19:13:23 -0300

    Feat: Api-Gateway añadida, falta testing y compilar los ms para Docker

--- Ms-usuario/src/main/java/com/usuario/Ms_usuario/MsUsuarioApplication.java
+++ Ms-usuario/src/main/java/com/usuario/Ms_usuario/MsUsuarioApplication.java
@@ -1,9 +1,10 @@
 package com.usuario.Ms_usuario;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsUsuarioApplication {
 
