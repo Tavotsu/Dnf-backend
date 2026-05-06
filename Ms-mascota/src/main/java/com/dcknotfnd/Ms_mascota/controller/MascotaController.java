19085fcfdedc69204b8f6abf1315c63746fcce10
Tavotsu <123342265+Tavotsu@users.noreply.github.com>
2026-05-06 13:57:53 -0400

    Feat: Missing dependencies & working with frontend

--- Ms-mascota/src/main/java/com/dcknotfnd/Ms_mascota/controller/MascotaController.java
+++ Ms-mascota/src/main/java/com/dcknotfnd/Ms_mascota/controller/MascotaController.java
@@ -2,6 +2,11 @@ package com.dcknotfnd.Ms_mascota.controller;
 
 import com.dcknotfnd.Ms_mascota.model.Mascota;
 import com.dcknotfnd.Ms_mascota.repository.MascotaRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
